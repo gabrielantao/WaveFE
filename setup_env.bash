98a24194033641f392864c8c96cf3af19d4ff81e
Gabriel Antão <gabrielantao@poli.ufrj.br>
2024-02-26 21:20:14 -0300

    Configure dependencies of simulator and add pixi task

--- setup_env.bash
+++ setup_env.bash
@@ -1,5 +1,5 @@
-# general paths
-export WAVE_PATH_ROOT=$(pwd)
+# put the modules in the the Julia path path
+export JULIA_LOAD_PATH="$PIXI_PACKAGE_ROOT:$JULIA_LOAD_PATH"
 
-# put the modules in the path
-export JULIA_LOAD_PATH="$WAVE_PATH_ROOT:$JULIA_LOAD_PATH"
+# put the test packages in the Julia path
+export JULIA_LOAD_PATH="$PIXI_PACKAGE_ROOT/simulator/test:$JULIA_LOAD_PATH"
